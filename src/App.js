ae27b94a281310b1139b8edd8ff4fee89efd1864
abruckenstein <abruckenstein@tradepmr.com>
2019-03-16 01:03:56 -0400

    holy shit, we're getting values. Jesus

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fire from './fire'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { messages: [] }; // <- set up react state
+  }
+
+  componentWillMount(){
+    
+    //var userId = fire.auth().currentUser.uid;
+    //var x = new Date().getTime();
+    //fire.database().ref('/').push({time: x}).then(console.log('done'))
+    // var ref = fire.database().ref('some/path');
+    // var obj = {someAttribute: true};
+
+    // ref.push(obj);
+
+    var ref = fire.database().ref();
+    var usersRef = ref.child('users');
+    var user;
+    usersRef.on('value', function (snap) {
+      console.log(snap.val());
+});
+
+  }
+
   render() {
     return (
       <div className="App">
