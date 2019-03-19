b5761aca48cc6d1e730762a852624d5559c28ae8
Alan <abruckenstein@tradepmr.com>
2019-03-19 15:28:47 -0400

    Button now actually updates with endDateTime and creates new obj with startDateTime when clicked

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
 
     // ref.push(obj);
     var ref = fire.database().ref();
-    var usersRef = ref.child('RaptorAttacks');
-    usersRef.on('value',  (snap) => {
+    var datesRef = ref.child('RaptorAttacks');
+    datesRef.on('value', (snap) => {
       var latestEndDateTime = new Date(Object.values(snap.val()).sort((a, b) => a.endDateTime < b.endDateTime)[0].endDateTime);
       this.calcDays(latestEndDateTime);
     });
@@ -37,24 +37,32 @@ class App extends Component {
         </header>
         <div className='App-body'>
         {this.state.days}
-        <a href="something" className="Button">Reset Counter</a>
+        <button onClick={this.getCurrentKeyThenPush} className="Button">Reset Counter</button>
         </div>
       </div>
     );
   }
 
-  pushToDB() {
-    var one = {endDateTime: (new Date().setDate(new Date().getDate()-17)), startDateTime:(new Date().setDate(new Date().getDate()-20))};
-    var two = {endDateTime: (new Date().setDate(new Date().getDate()-20)), startDateTime:(new Date().setDate(new Date().getDate()-75))};
-    fire.database().ref('RaptorAttacks').push(two) //Adds object to RaptorAttacks, this is to add previous code red dates
+  pushToDB = (currentCountKey) => {
+    //var one = {endDateTime: (new Date().setDate(new Date().getDate()-17)), startDateTime:(new Date().setDate(new Date().getDate()-20))};
+    //var two = {endDateTime: (new Date().setDate(new Date().getDate()-20)), startDateTime:(new Date().setDate(new Date().getDate()-75))};
+    fire.database().ref('RaptorAttacks/' + currentCountKey).update({endDateTime: new Date().getTime()});
+    fire.database().ref('RaptorAttacks').push({startDateTime: new Date().getTime()});
+    //fire.database().ref('RaptorAttacks').push() //Adds object to RaptorAttacks, this is to add previous code red dates
     //Should get most recent raptor attack, and determine days since last by start date. Then when reset, add endate and create new entry
   }
 
-  calcDays(latestEndDateTime){
+  getCurrentKeyThenPush = () => {
+    var ref = fire.database().ref();
+    var datesRef = ref.child('RaptorAttacks');
+    datesRef.limitToLast(1).once('value', (snap) => {this.pushToDB(Object.keys(snap.val())[0])})
+  }
+
+  calcDays = (latestEndDateTime) => {
     
     const oneDay = 24*60*60*1000;
     var today = new Date();
-    var daysSinceLastAttack =  Math.round(Math.abs(((today.getTime() - latestEndDateTime)/(oneDay))))
+    var daysSinceLastAttack = Math.round(Math.abs(((today.getTime() - latestEndDateTime)/(oneDay))))
     this.setState({days: daysSinceLastAttack})
   }
 }
