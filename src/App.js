3f70f6532e16d993a2b54c258dec0660154636b0
Alan <abruckenstein@tradepmr.com>
2019-03-19 15:54:02 -0400

    added old date values

--- src/App.js
+++ src/App.js
@@ -12,18 +12,12 @@ class App extends Component {
   }
 
   componentWillMount() {
-
-    //var userId = fire.auth().currentUser.uid;
-
-    // ref.push(obj);
     var ref = fire.database().ref();
     var datesRef = ref.child('RaptorAttacks');
     datesRef.on('value', (snap) => {
       var latestEndDateTime = new Date(Object.values(snap.val()).sort((a, b) => a.endDateTime < b.endDateTime)[0].endDateTime);
       this.calcDays(latestEndDateTime);
-    });
-    //this.pushToDB();
-    
+    });    
   }
 
   render() {
@@ -32,7 +26,7 @@ class App extends Component {
         <header className='App-header'>
           <img src={raptor} alt='' className='Header-image'></img>
           <p>
-            Days since last raptor attack :
+            Days since last raptor attack:
           </p>
         </header>
         <div className='App-body'>
@@ -44,12 +38,9 @@ class App extends Component {
   }
 
   pushToDB = (currentCountKey) => {
-    //var one = {endDateTime: (new Date().setDate(new Date().getDate()-17)), startDateTime:(new Date().setDate(new Date().getDate()-20))};
-    //var two = {endDateTime: (new Date().setDate(new Date().getDate()-20)), startDateTime:(new Date().setDate(new Date().getDate()-75))};
+    
     fire.database().ref('RaptorAttacks/' + currentCountKey).update({endDateTime: new Date().getTime()});
     fire.database().ref('RaptorAttacks').push({startDateTime: new Date().getTime()});
-    //fire.database().ref('RaptorAttacks').push() //Adds object to RaptorAttacks, this is to add previous code red dates
-    //Should get most recent raptor attack, and determine days since last by start date. Then when reset, add endate and create new entry
   }
 
   getCurrentKeyThenPush = () => {
@@ -59,11 +50,24 @@ class App extends Component {
   }
 
   calcDays = (latestEndDateTime) => {
-    
     const oneDay = 24*60*60*1000;
     var today = new Date();
     var daysSinceLastAttack = Math.round(Math.abs(((today.getTime() - latestEndDateTime)/(oneDay))))
     this.setState({days: daysSinceLastAttack})
+  }
+
+  resetPastDates = () => {
+    var one = {endDateTime: (new Date().setDate(new Date().getDate()-18)), startDateTime:(new Date().setDate(new Date().getDate()-21))};
+    var two = {endDateTime: (new Date().setDate(new Date().getDate()-21)), startDateTime:(new Date().setDate(new Date().getDate()-76))};
+    var three = {endDateTime: (new Date().setDate(new Date().getDate()-76)), startDateTime:(new Date().setDate(new Date().getDate()-99))};
+    var four = {endDateTime: (new Date().setDate(new Date().getDate()-99)), startDateTime:(new Date().setDate(new Date().getDate()-130))};
+    var five = {endDateTime: (new Date().setDate(new Date().getDate()-130)), startDateTime:(new Date().setDate(new Date().getDate()-137))};
+    var six = {endDateTime: (new Date().setDate(new Date().getDate()-137)), startDateTime:(new Date().setDate(new Date().getDate()-144))};
+    var seven = {endDateTime: (new Date().setDate(new Date().getDate()- 144)), startDateTime:(new Date().setDate(new Date().getDate()-151))};
+    var eight = {endDateTime: (new Date().setDate(new Date().getDate()-151)), startDateTime:(new Date().setDate(new Date().getDate()-158))};
+    var nine = {endDateTime: (new Date().setDate(new Date().getDate()-158)), startDateTime:(new Date().setDate(new Date().getDate()-192))};
+    var whatever = [one, two, three, four, five, six, seven, eight, nine];
+    whatever.forEach(function(obj) {fire.database().ref('RaptorAttacks').push(obj)})
   }
 }
 
